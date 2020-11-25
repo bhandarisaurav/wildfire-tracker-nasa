39cffbff7f3950afd4c1fbf7787b21247ae47c96
bhandarisaurav <saurav.bhandari@yahoo.com>
2020-11-25 23:14:13 +0545

    :key: Added NASA api authorization key

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -1,4 +1,4 @@
-// const URL = `https://eonet.sci.gsfc.nasa.gov/api/v2.1/events`;
-const URL = 'http://127.0.0.1:5500/src/components/utils/data.json';
+const API_KEY = 'qJZA5ZM3xLg4nARkHqdsbZiy4qCJfXhSoRjQPwrd';
+const URL = `https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?api_key=${API_KEY}`;
 
 export { URL };
